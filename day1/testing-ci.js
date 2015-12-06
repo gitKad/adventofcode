4954953b01e041961a1cb4b0bf198083cfa42c8e
unknown <alexisp@AlexisP-PRO3.tecsys.corp>
2015-12-06 16:09:18 -0500

    first test to validate if tests and CI work

---
+++ day1/testing-ci.js
@@ -0,0 +1,10 @@
+var expect = require('chai').expect;
+
+// from ericmigno@github
+describe('Testing environment', function () {
+
+    it('is ready to use chai with mocha', function () {
+        expect(1 + 1).to.equal(2);
+    });
+
+});
