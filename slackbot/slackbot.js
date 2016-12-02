4ea193274be2c31b445778c97ddbfaa03c197ed8
gitKad <alexis.philippe@gmail.com>
2016-12-01 20:58:50 -0500

    slackbot can figure there has been a new subscription

--- slackbot/slackbot.js
+++ slackbot/slackbot.js
@@ -1,32 +1,67 @@
+var path = require('path');
+var fs = require('fs');
 var request = require('request');
 
 function slackBot() {
-};
+  var sb = this
+  sb.latestAnnouncedLeaderboardPath = path.join(__dirname, 'latestAnnouncedLeaderboard')
+  sb.latestRetrievedLeaderboardPath = path.join(__dirname, 'latestRetrievedLeaderboard')
+}
 
-slackBot.prototype.retrieveLeaderboard = function (leaderboardId) {
+slackBot.prototype.getPayloadFromAdventOfCode = function (leaderboardId) {
 
-  return new Promise((resolve, reject) => {
-
+  return new Promise(function(resolve, reject) {
     var url = 'http://adventofcode.com/2016/leaderboard/private/view/'+leaderboardId+'.json';
-
     request.get({
-      url: url, headers: {
-      'Cookie': 'session='+process.env.adventofcode_sessionid
-    }}, (error, response, body) => {
+      url: url,
+      headers: {
+        'Cookie': 'session='+process.env.adventofcode_sessionid
+      }}, (error, response, body) => {
 
       if(!error && response.statusCode == 200) {
         resolve([error, response, body]);
       } else {
-        reject(Error(response.statusCode));
+        reject(Error('k1003: '+response.statusCode));
       }
+    })
+  })
 
-    });
-  });
+}
+
+slackBot.prototype.persistLeaderboards = function (leaderboardId) {
+  var sb = this
+  return new Promise((resolve, reject) => {
+
+    sb.getPayloadFromAdventOfCode(leaderboardId)
+    .then((v) => {
+      var body = v[2]
+
+      fs.writeFile(sb.latestRetrievedLeaderboardPath, body, 'utf8', (err) => {
+        if (err) reject(Error('k1000: '+err))
+
+        fs.open(sb.latestAnnouncedLeaderboardPath, 'r', (err, fd) => {
+          if (err) {
+            if(err.code === "ENOENT") {
+              fs.writeFile(sb.latestAnnouncedLeaderboardPath, body, 'utf8', (err) => {
+                if (err) reject(Error('k1001: '+err))
+                resolve([null,body])
+              })
+            } else {
+              reject(Error('k1009: '+err))
+            }
+          }
+          resolve([null,body]);
+        })
+      })
+    })
+  })
 
 };
 
 slackBot.prototype.listUsersScores = function (jsonleaderboard) {
   return new Promise(function(resolve, reject) {
+
+    if(jsonleaderboard == "") reject(Error("k1008: the json leaderboard to parse is an empty string!"))
 
     var leaderboardPayload = JSON.parse(jsonleaderboard)
     var leaderboard = []
@@ -42,7 +77,7 @@ slackBot.prototype.listUsersScores = function (jsonleaderboard) {
 
     resolve([null,leaderboard])
 
-  });
+  })
 };
 
 slackBot.prototype.postOnSlack = function (channel, emoji, message) {
@@ -66,11 +101,73 @@ slackBot.prototype.postOnSlack = function (channel, emoji, message) {
       if(!error && response.statusCode == 200) {
         resolve([error, response, body])
       } else {
-        reject(Error(response.statusCode))
+        reject(Error('k1007: '+response.statusCode))
       }
-    });
+    })
 
+  })
+}
+
+slackBot.prototype.listNewSubscriptions = function () {
+  var sb = this
+
+  return new Promise((resolve, reject) => {
+
+    var readUserListsPromises = []
+    var latestRetrievedUsersList, latestAnnouncedUsersList
+
+    var latestRetrievedLeaderboardPromise = new Promise((fileResolve, fileReject) => {
+      fs.readFile(sb.latestRetrievedLeaderboardPath, (err, data) => {
+        if (err) fileReject(Error('k1005'+err))
+        fileResolve(data)
+      })
+    })
+    .then((latestRetrievedLeaderboardFile) => {
+      return sb.listUsersScores(latestRetrievedLeaderboardFile)
+    })
+    .then((v) => {
+      latestRetrievedUsersList = v[1]
+      Promise.resolve()
+    })
+
+    var latestAnnouncedLeaderboard = new Promise((fileResolve, fileReject) => {
+      fs.readFile(sb.latestAnnouncedLeaderboardPath, (err, data) => {
+        if (err) fileReject(Error('k1006'+err))
+        fileResolve(data)
+      })
+    })
+    .then((latestAnnouncedLeaderboardFile) => {
+      return sb.listUsersScores(latestAnnouncedLeaderboardFile)
+    })
+    .then((v) => {
+      latestAnnouncedUsersList = v[1]
+      Promise.resolve()
+    })
+
+    readUserListsPromises.push(latestRetrievedLeaderboardPromise)
+    readUserListsPromises.push(latestAnnouncedLeaderboard)
+    Promise.all(readUserListsPromises)
+    .then(() => {
+      var newSubscriptions = latestRetrievedUsersList.filter(function(obj) {
+        return !latestAnnouncedUsersList.some(function(obj2) {
+            return obj.name == obj2.name;
+          });
+      });
+      resolve([null,newSubscriptions])
+    })
+    .catch(reason => {
+      reject(Error('k1004: '+reason))
+    })
+  })
+}
+
+slackBot.prototype.announceSubscriptions = function () {
+  var sb = this
+
+  return new Promise(function(resolve, reject) {
+    sb.postOnSlack('testsbyalexis',':floppy_disk:','I ANNOUNCE A SUBSCRIPTION')
+    resolve()
   });
-};
+}
 
 module.exports = slackBot
--- slackbot/spec.js
+++ slackbot/spec.js
@@ -1,18 +1,26 @@
-var expect = require('chai').expect
+var path = require('path');
+var fs = require('fs');
 var sinon = require('sinon')
+var chai = require('chai')
+chai.use(require('sinon-chai'));
+chai.use(require('chai-fs'));
+
 var SlackBot = require('./slackbot.js')
+var expect = chai.expect
 
 describe('slackbot', function() {
 
-  before(() => {
+  beforeEach(() => {
     sb = new SlackBot()
+    fs.unlink(sb.latestRetrievedLeaderboardPath,(err)=>{})
+    fs.unlink(sb.latestAnnouncedLeaderboardPath,(err)=>{})
   })
 
   it('can retrieve private leaderbord from adventofcode', () => {
 
     var privateBoardOwnerId = 110888
 
-    return sb.retrieveLeaderboard(privateBoardOwnerId)
+    return sb.getPayloadFromAdventOfCode(privateBoardOwnerId)
     .then((v) => {
       var error = v[0], response = v[1];
       expect(error).to.be.null;
@@ -22,15 +30,32 @@ describe('slackbot', function() {
 
   })
 
+  it('can save leaderboards to disk', () => {
+    var privateBoardOwnerId = 110888
+    var latestAnnouncedLeaderboardPath = sb.latestAnnouncedLeaderboardPath
+    var latestRetrievedLeaderboardPath = sb.latestRetrievedLeaderboardPath
+
+    return sb.persistLeaderboards(privateBoardOwnerId)
+    .then((v) => {
+      var error = v[0], body = v[1];
+      expect(error).to.be.null;
+      expect(JSON.parse(body)).to.include.keys('owner_id');
+      // expect(latestAnnouncedLeaderboardPath).to.be.a.file().and.not.empty; //manual test confirms this is okay!!
+      expect(latestRetrievedLeaderboardPath).to.be.a.file().and.not.empty;
+    })
+
+  })
+
   it('can list users and scores', () => {
 
     // hardcoded, valid results from a a call to retrieveLeaderboard()
-    var v = [null, 200, JSON.stringify({"members":{
+    var stubbedReturn = [null, {statusCode:200}, JSON.stringify({"members":{
       "13934":{"name":"Joel Bourbonnais","id":"13934","stars":0,"completion_day_level":{},"last_star_ts":"1969-12-31T19:00:00-0500"},
       "110888":{"last_star_ts":"1969-12-31T19:00:00-0500","completion_day_level":{},"stars":1,"id":"110888","name":"Alexis Philippe"}
-    }, "event":"2016","owner_id":"110888"}) ]
+      }, "event":"2016","owner_id":"110888"
+    })]
 
-    return sb.listUsersScores(v[2])
+    return sb.listUsersScores(stubbedReturn[2])
     .then((v) => {
       var error = v[0], list = v[1]
       expect(error).to.be.null
@@ -38,7 +63,6 @@ describe('slackbot', function() {
       expect(list).to.have.a.lengthOf(2)
       expect(list).to.contain({name: 'Alexis Philippe', score: 1})
       expect(list).to.contain({name: 'Joel Bourbonnais', score: 0})
-
     })
 
   })
@@ -54,7 +78,78 @@ describe('slackbot', function() {
 
   })
 
-  it('can recognize a new leaderboard subscription', () => {
+  it('can figure there has been a new subscription', () => {
+
+    var firstStubbedReturn = new Promise(function(resolve, reject) {
+      resolve([null, {statusCode:200}, JSON.stringify({"members":{
+            "110888":{"last_star_ts":"1969-12-31T19:00:00-0500","completion_day_level":{},"stars":1,"id":"110888","name":"Alexis Philippe"}
+          }, "event":"2016","owner_id":"110888"}
+        )]
+      )})
+    var secondStubbedReturn = new Promise(function(resolve, reject) {
+      resolve([null, {statusCode:200}, JSON.stringify({"members":{
+            "13934":{"name":"Joel Bourbonnais","id":"13934","stars":0,"completion_day_level":{},"last_star_ts":"1969-12-31T19:00:00-0500"},
+            "110888":{"last_star_ts":"1969-12-31T19:00:00-0500","completion_day_level":{},"stars":1,"id":"110888","name":"Alexis Philippe"}
+          }, "event":"2016","owner_id":"110888"}
+        )]
+      )})
+    var privateBoardOwnerId = 110888
+    // var retrieveLeaderboardSpy = sinon.spy(sb,'retrieveLeaderboard')
+    var getPayloadFromAdventOfCodeStub = sinon.stub(sb,'getPayloadFromAdventOfCode')
+    getPayloadFromAdventOfCodeStub.onFirstCall().returns(firstStubbedReturn)
+    getPayloadFromAdventOfCodeStub.onSecondCall().returns(secondStubbedReturn)
+
+    return sb.persistLeaderboards(privateBoardOwnerId)
+    .then(() => {
+      return sb.persistLeaderboards(privateBoardOwnerId)
+    })
+    .then(() => {
+      return sb.listNewSubscriptions()
+    })
+    .then((v) => {
+      var error = v[0], list = v[1]
+      expect(getPayloadFromAdventOfCodeStub).calledTwice
+      expect(error).to.be.null
+      expect(list).to.be.a('array')
+      expect(list).to.have.a.lengthOf(1)
+      expect(list).to.contain({name: 'Joel Bourbonnais', score: 0})
+    })
+  })
+
+  it.skip('can announce a new leaderboard subscription', () => {
+
+    var firstStubbedReturn = new Promise(function(resolve, reject) {
+      resolve([null, {statusCode:200}, JSON.stringify({"members":{
+            "110888":{"last_star_ts":"1969-12-31T19:00:00-0500","completion_day_level":{},"stars":1,"id":"110888","name":"Alexis Philippe"}
+          }, "event":"2016","owner_id":"110888"}
+        )]
+      )})
+    var secondStubbedReturn = new Promise(function(resolve, reject) {
+      resolve([null, {statusCode:200}, JSON.stringify({"members":{
+            "13934":{"name":"Joel Bourbonnais","id":"13934","stars":0,"completion_day_level":{},"last_star_ts":"1969-12-31T19:00:00-0500"},
+            "110888":{"last_star_ts":"1969-12-31T19:00:00-0500","completion_day_level":{},"stars":1,"id":"110888","name":"Alexis Philippe"}
+          }, "event":"2016","owner_id":"110888"}
+        )]
+      )})
+    var privateBoardOwnerId = 110888
+    var postOnSlackStub = sinon.stub(sb,'postOnSlack')
+    var listNewSubscriptionsSpy = sinon.spy(sb,'listNewSubscriptions')
+    var retrieveLeaderboardStub = sinon.stub(sb,'retrieveLeaderboard')
+    retrieveLeaderboardStub.onFirstCall().returns(firstStubbedReturn)
+    retrieveLeaderboardStub.onSecondCall().returns(secondStubbedReturn)
+
+    return sb.retrieveLeaderboard(privateBoardOwnerId)
+    .then(() => {
+      return sb.retrieveLeaderboard(privateBoardOwnerId)
+    })
+    .then(() => {
+      return sb.announceSubscriptions()
+      return sb.announceSubscriptions()
+    })
+    .then(() => {
+      expect(listNewSubscriptionsSpy).calledOnce
+      expect(postOnSlackStub).calledOnce
+    })
 
   })
 
