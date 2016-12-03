3ad3e087aff172a184c1a9529358d45b6ac9df0d
gitKad <alexis.philippe@gmail.com>
2016-12-03 18:59:22 -0500

    strong refactor of input lurker to minimize dependencies

--- 2016/adventOfCodeChatter.js
+++ 2016/adventOfCodeChatter.js
@@ -1,42 +1,51 @@
-var request = require('request-promise')
+var http = require('http')
 
 var adventOfCodeChatter = function() {
 
 }
 
 adventOfCodeChatter.prototype.getInput = function (day) {
-  var sessionID = process.env.adventofcode_sessionId;
-
   var options = {
-    url: 'http://adventofcode.com/2016/day/'+day+'/input',
+    hostname: 'adventofcode.com',
+    port: 80,
+    path: '/2016/day/'+day+'/input',
+    method: 'GET',
     headers: {
-      'Cookie': 'session='+sessionID
+      'Cookie': 'session='+process.env.adventofcode_sessionId
     }
   }
 
-  return request(options).then( (body) => {
-    return Promise.resolve(body)
+  return new Promise((resolve, reject) => {
+    var request = http.get(options, (response) => {
+      if (response.statusCode < 200 || response.statusCode > 299) {
+         reject(new Error('Failed to load page, status code: ' + response.statusCode));
+      }
+      const body = [];
+      response.on('data', (chunk) => body.push(chunk));
+      response.on('end', () => resolve(body.join('')));
+    })
+    request.on('error', (err) => reject(err))
   })
 }
 
-adventOfCodeChatter.prototype.postSolution = function (day, solution, callback) {
-  var sessionID = process.env.adventofcode_sessionId;
-
-  var options = {
-    url: 'http://adventofcode.com/2016/day/'+day+'/answer',
-    form: {
-      'level': day,
-      'answer':solution
-    },
-    headers: {
-      'Cookie': 'session='+sessionID
-    }
-  }
-
-  request.post(options).then( function(body){
-    callback(body);
-  })
-}
+// adventOfCodeChatter.prototype.postSolution = function (day, solution, callback) {
+//   var sessionID = process.env.adventofcode_sessionId;
+//
+//   var options = {
+//     url: 'http://adventofcode.com/2016/day/'+day+'/answer',
+//     form: {
+//       'level': day,
+//       'answer':solution
+//     },
+//     headers: {
+//       'Cookie': 'session='+sessionID
+//     }
+//   }
+//
+//   request.post(options).then( function(body){
+//     callback(body);
+//   })
+// }
 
 
 module.exports = adventOfCodeChatter
