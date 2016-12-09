186224a1b68bd67b2118c2a92dbc8821c5bec7df
gitKad <alexis.philippe@gmail.com>
2016-12-08 23:38:23 -0500

    try if request will build on travis instead of pure node

--- 2016/adventOfCodeChatter.js
+++ 2016/adventOfCodeChatter.js
@@ -4,12 +4,11 @@ var adventOfCodeChatter = function() {
 
 }
 
+  var request = require('request')
+
 adventOfCodeChatter.prototype.getInput = function (day) {
   var options = {
-    hostname: 'adventofcode.com',
-    port: 80,
-    path: '/2016/day/'+day+'/input',
-    method: 'GET',
+    url: 'http://www.adventofcode.com/2016/day/'+day+'/input',
     headers: {
       'Cookie': 'session='+process.env.adventofcode_sessionId,
       'user-agent': 'node.js'
@@ -17,17 +16,40 @@ adventOfCodeChatter.prototype.getInput = function (day) {
   }
 
   return new Promise((resolve, reject) => {
-    var request = http.get(options, (response) => {
-      if (response.statusCode < 200 || response.statusCode > 299) {
-         reject(new Error('Failed to load page, status code: ' + response.statusCode));
+    request(options, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        resolve(body)
+      } else {
+        reject(error)
       }
-      const body = [];
-      response.on('data', (chunk) => body.push(chunk));
-      response.on('end', () => resolve(body.join('')));
     })
-    request.on('error', (err) => reject(err))
   })
 }
+
+// adventOfCodeChatter.prototype.getInput = function (day) {
+//   var options = {
+//     hostname: 'adventofcode.com',
+//     port: 80,
+//     path: '/2016/day/'+day+'/input',
+//     method: 'GET',
+//     headers: {
+//       'Cookie': 'session='+process.env.adventofcode_sessionId,
+//       'user-agent': 'node.js'
+//     }
+//   }
+//
+//   return new Promise((resolve, reject) => {
+//     var request = http.get(options, (response) => {
+//       if (response.statusCode < 200 || response.statusCode > 299) {
+//          reject(new Error('Failed to load page, status code: ' + response.statusCode));
+//       }
+//       const body = [];
+//       response.on('data', (chunk) => body.push(chunk));
+//       response.on('end', () => resolve(body.join('')));
+//     })
+//     request.on('error', (err) => reject(err))
+//   })
+// }
 
 // adventOfCodeChatter.prototype.postSolution = function (day, solution, callback) {
 //   var sessionID = process.env.adventofcode_sessionId;
