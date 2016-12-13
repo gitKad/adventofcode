fbeb2ac35e565550bf331a9e30b489e8daed2b60
gitKad <alexis.philippe@gmail.com>
2016-12-13 09:50:20 -0500

    next day template

--- 2016/day6.js
+++ 2016/day6.js
@@ -7,23 +7,23 @@ chai.use(chaiAsPromised);
 var Me = require('./me.js')
 var AdventOfCodeChatter = require('./adventOfCodeChatter.js')
 
-describe('I, on day 5, ', () => {
+describe.skip('I, on day 6, ', () => {
 
   beforeEach(() => {
     me = new Me()
   })
 
-  it.skip('can earn a silver star on day 5', () => {
+  it.skip('can earn a silver star on day 6', () => {
     var aCC = new AdventOfCodeChatter()
-    return aCC.getInput(5)
+    return aCC.getInput(6)
     .then((input) => {
       expect(input).to.be.a('number').equal(1050)
     })
   })
 
-  it.skip('can earn a gold star on day 5', () => {
+  it.skip('can earn a gold star on day 6', () => {
     var aCC = new AdventOfCodeChatter()
-    return aCC.getInput(5)
+    return aCC.getInput(6)
     .then((input) => {
       expect(input).to.be.a('number').equal(1921)
     })
