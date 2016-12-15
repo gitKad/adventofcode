2e984235970dc27b105666923f2c31e07ee23459
gitKad <alexis.philippe@gmail.com>
2016-12-15 16:15:42 -0500

    created day 7

---
+++ 2016/day7.js
@@ -0,0 +1,32 @@
+var chai = require('chai')
+var expect = require('chai').expect
+var chaiAsPromised = require("chai-as-promised")
+
+chai.use(chaiAsPromised);
+
+var Me = require('./me.js')
+var AdventOfCodeChatter = require('./adventOfCodeChatter.js')
+
+describe.skip('I, on day 7, ', () => {
+
+  beforeEach(() => {
+    me = new Me()
+  })
+
+  it.skip('can earn a silver star on day 6', () => {
+    var aCC = new AdventOfCodeChatter()
+    return aCC.getInput(7)
+    .then((input) => {
+      expect(input).to.be.a('number').equal(1050)
+    })
+  })
+
+  it.skip('can earn a gold star on day 6', () => {
+    var aCC = new AdventOfCodeChatter()
+    return aCC.getInput(7)
+    .then((input) => {
+      expect(input).to.be.a('number').equal(1921)
+    })
+  })
+
+})
