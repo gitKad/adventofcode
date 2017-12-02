24a192e8b96e4ad28eaf8e4141e80fb3ac8c6638
Alexis Philippe <alexis.philippe@gmail.com>
2017-12-02 13:54:09 -0500

    refactored in smaller units of code

--- 2017/me.js
+++ 2017/me.js
@@ -6,21 +6,30 @@ var Me = function() {
 }
 
 Me.prototype.resolveSimpleCaptcha = function (captcha) {
-    return captcha
-      .replace(/\n|\r/g, '')
-      .split('')
-      .map(e => parseInt(e))
-      .filter((element, index, array) => (element == array[(index+1) % array.length]))
-      .reduce((sum, current) => (sum + current), 0)
-  }
+  return parseStringIntoCaptcha(captcha)
+    .filter(matchingNextArrayDigit)
+    .reduce((sum, current) => (sum + current), 0)
+}
 
 Me.prototype.resolveComplexCaptcha = function (captcha) {
+  return parseStringIntoCaptcha(captcha)
+    .filter(matchingHalfwayRoundDigit)
+    .reduce((sum, current) => (sum + current), 0)
+}
+
+function parseStringIntoCaptcha(captcha) {
   return captcha
     .replace(/\n|\r/g, '')
     .split('')
     .map(e => parseInt(e))
-    .filter((element, index, array) => (element == array[(index+ array.length / 2) % array.length]))
-    .reduce((sum, current) => (sum + current), 0)
+}
+
+function matchingNextArrayDigit(element, index, array) {
+  return element == array[(index+1) % array.length]
+}
+
+function matchingHalfwayRoundDigit(element, index, array) {
+  return element == array[(index+ array.length / 2) % array.length]
 }
 
 module.exports = Me
