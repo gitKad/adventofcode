9e8682ded8fc27ec8b21f3ed44aa07961731983c
Alexis Philippe <alexis.philippe@gmail.com>
2017-12-03 13:22:39 -0500

    day2 gold star

--- 2017/day2.js
+++ 2017/day2.js
@@ -12,24 +12,33 @@ describe('I, on day '+day+', ', () => {
 
   beforeEach(() => {
     me = new Me()
-    exampleSpreadsheet = '5\t1\t9\t5\n7\t5\t3\n2\t4\t6\t8\n'
   })
 
-  it('can tell the difference between max and min in a spreadsheet row', () => {
+  it('can tell the difference between max and min numbers in array', () => {
     expect(me.differenceOfMinAndMax([5, 1, 9, 5])).to.be.eql(8)
     expect(me.differenceOfMinAndMax([7, 5, 3])).to.be.eql(4)
     expect(me.differenceOfMinAndMax([2, 4, 6, 8])).to.be.eql(6)
   })
 
+  it('can tell the division between evenly divisible numbers in array', () => {
+    expect(me.divisionOfEvenlyDivisible([5, 9, 2, 8])).to.be.eql(4)
+    expect(me.divisionOfEvenlyDivisible([9, 4, 7, 3])).to.be.eql(3)
+    expect(me.divisionOfEvenlyDivisible([3, 8, 6, 5])).to.be.eql(2)
+  })
+
   it('can resolve simple spreadsheet checksum I was given as examples', () => {
-    expect(me.calculateChecksum(exampleSpreadsheet)).to.be.eql(18)
+    expect(me.calculateMinMaxChecksum('5\t1\t9\t5\n7\t5\t3\n2\t4\t6\t8\n')).to.be.eql(18)
+  })
+
+  it('can resolve complex spreadsheet checksum I was given as examples', () => {
+    expect(me.calculateEvenlyDivisibleChecksum('5\t9\t2\t8\n9\t4\t7\t3\n3\t8\t6\t5\n')).to.be.eql(9)
   })
 
   it('can earn a silver star on day '+day, () => {
     var aCC = new AdventOfCodeChatter()
     return aCC.getInput(day)
     .then((input) => {
-      expect(me.calculateChecksum(input)).to.be.eql(47136)
+      expect(me.calculateMinMaxChecksum(input)).to.be.eql(47136)
     })
   })
 
@@ -37,7 +46,7 @@ describe('I, on day '+day+', ', () => {
     var aCC = new AdventOfCodeChatter()
     return aCC.getInput(day)
     .then((input) => {
-      expect(input).to.be.ok
+      expect(me.calculateEvenlyDivisibleChecksum(input)).to.be.eql(250)
     })
   })
 
--- 2017/me.js
+++ 2017/me.js
@@ -6,9 +6,15 @@ var Me = function() {
 }
 
 // Day 2 stuff
-Me.prototype.calculateChecksum = function(spreadsheet) {
+Me.prototype.calculateMinMaxChecksum = function(spreadsheet) {
   return parseStringIntoSpreadsheet(spreadsheet)
     .map( this.differenceOfMinAndMax )
+    .reduce((sum, current) => (sum+current), 0)
+}
+
+Me.prototype.calculateEvenlyDivisibleChecksum = function(spreadsheet) {
+  return parseStringIntoSpreadsheet(spreadsheet)
+    .map( this.divisionOfEvenlyDivisible )
     .reduce((sum, current) => (sum+current), 0)
 }
 
@@ -19,8 +25,17 @@ function parseStringIntoSpreadsheet(spreadsheet) {
   .map((current) => {
     return current
       .split(/\t/g)
-      .map(e => parseInt(e))
+      .map(Number)
   })
+}
+
+Me.prototype.divisionOfEvenlyDivisible = function(cur) {
+  return cur
+    .filter((element, _, array) => {
+      return array
+        .filter( (c) => c != element && (element%c == 0 || c%element == 0), 0 ).length > 0
+    })
+    .reduce((a, b) => b > a ? b/a : a/b)
 }
 
 Me.prototype.differenceOfMinAndMax = function(cur) {
