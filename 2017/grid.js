d0b80e6b6afb931b83e294cc6382eacbe33e14f5
Alexis Philippe <alexis.philippe@gmail.com>
2017-12-04 14:20:36 -0500

    ES6 type classes

--- 2017/grid.js
+++ 2017/grid.js
@@ -2,41 +2,45 @@
 
 var Promise = require('bluebird')
 
-var Grid = function() {
-}
+class Grid {
+  constructor () {
 
-Grid.prototype.measureStepsToCenter = function(square) {
-  let squareSize = this.getSquareRingSize(square)
-  let maxDistanceToCenter = this.getRingMaxDistanceFromCenter(squareSize)
-  let minDistanceToCenter = this.getRingMinDistanceFromCenter(squareSize)
-
-  var distance = maxDistanceToCenter
-  var direction = -1
-  for(let i = squareSize**2; i > square; i--) {
-    if(distance == minDistanceToCenter) {
-      direction = 1
-    }
-    else if(distance == maxDistanceToCenter){
-      direction = -1
-    }
-    distance += direction
   }
 
-  return distance
-}
+  measureStepsToCenter (square) {
+    let squareSize = this.getSquareRingSize(square)
+    let maxDistanceToCenter = this.getRingMaxDistanceFromCenter(squareSize)
+    let minDistanceToCenter = this.getRingMinDistanceFromCenter(squareSize)
+  
+    var distance = maxDistanceToCenter
+    var direction = -1
+    for(let i = squareSize**2; i > square; i--) {
+      if(distance == minDistanceToCenter) {
+        direction = 1
+      }
+      else if(distance == maxDistanceToCenter){
+        direction = -1
+      }
+      distance += direction
+    }
+  
+    return distance
+  }
+  
+  getSquareRingSize (square) {
+    let squareSize = Math.ceil(Math.sqrt(square))
+    squareSize += squareSize % 2 == 0 ? 1 : 0
+    return squareSize
+  }
 
-Grid.prototype.getSquareRingSize = function(square) {
-  let squareSize = Math.ceil(Math.sqrt(square))
-  squareSize += squareSize % 2 == 0 ? 1 : 0
-  return squareSize
-}
+  getRingMinDistanceFromCenter (squareSize) {
+    return (squareSize - 1) / 2
+  }
 
-Grid.prototype.getRingMinDistanceFromCenter = function(squareSize) {
-  return (squareSize - 1) / 2
-}
+  getRingMaxDistanceFromCenter (squareSize) {
+    return (squareSize - 1)
+  }
 
-Grid.prototype.getRingMaxDistanceFromCenter = function(squareSize) {
-  return (squareSize - 1)
 }
 
 module.exports = Grid
--- 2017/me.js
+++ 2017/me.js
@@ -2,76 +2,79 @@
 
 var Promise = require('bluebird')
 
-var Me = function() {
-}
+class Me {
+  constructor() {
 
-Me.prototype.measureStepsToCenter = function(grid, square) {
-  return grid.measureStepsToCenter(square)
-}
+  }
 
-// Day 2 stuff
-Me.prototype.calculateMinMaxChecksum = function(spreadsheet) {
-  return parseStringIntoSpreadsheet(spreadsheet)
-    .map( this.differenceOfMinAndMax )
-    .reduce((sum, current) => (sum+current), 0)
-}
+  // Day 3 stuff
+  measureStepsToCenter(grid, square) {
+    return grid.measureStepsToCenter(square)
+  }
+  // Day 2 stuff
+  calculateMinMaxChecksum(spreadsheet) {
+    return this.parseStringIntoSpreadsheet(spreadsheet)
+      .map( this.differenceOfMinAndMax )
+      .reduce((sum, current) => (sum+current), 0)
+  }
 
-Me.prototype.calculateEvenlyDivisibleChecksum = function(spreadsheet) {
-  return parseStringIntoSpreadsheet(spreadsheet)
-    .map( this.divisionOfEvenlyDivisible )
-    .reduce((sum, current) => (sum+current), 0)
-}
+  calculateEvenlyDivisibleChecksum(spreadsheet) {
+    return this.parseStringIntoSpreadsheet(spreadsheet)
+      .map( this.divisionOfEvenlyDivisible )
+      .reduce((sum, current) => (sum+current), 0)
+  }
 
-function parseStringIntoSpreadsheet(spreadsheet) {
-  return spreadsheet
-  .replace(/\n$/gm,'')
-  .split(/\n/g)
-  .map((current) => {
-    return current
-      .split(/\t/g)
-      .map(e => parseInt(e))
-  })
-}
-
-Me.prototype.divisionOfEvenlyDivisible = function(cur) {
-  return cur
-    .filter((element, _, array) => {
-      return array
-        .filter( (c) => c != element && (element%c == 0 || c%element == 0), 0 ).length > 0
+  parseStringIntoSpreadsheet(spreadsheet) {
+    return spreadsheet
+    .replace(/\n$/gm,'')
+    .split(/\n/g)
+    .map((current) => {
+      return current
+        .split(/\t/g)
+        .map(e => parseInt(e))
     })
-    .reduce((a, b) => b > a ? b/a : a/b)
-}
+  }
 
-Me.prototype.differenceOfMinAndMax = function(cur) {
-  return Math.max(...cur) - Math.min(...cur)
-}
+  divisionOfEvenlyDivisible(cur) {
+    return cur
+      .filter((element, _, array) => {
+        return array
+          .filter( (c) => c != element && (element%c == 0 || c%element == 0), 0 ).length > 0
+      })
+      .reduce((a, b) => b > a ? b/a : a/b)
+  }
 
-// Day 1 stuff
-Me.prototype.solveSimpleCaptcha = function (captcha) {
-  return parseStringIntoCaptcha(captcha)
-    .filter(matchingNextArrayDigit)
-    .reduce((sum, current) => (sum + current), 0)
-}
+  differenceOfMinAndMax(cur) {
+    return Math.max(...cur) - Math.min(...cur)
+  }
 
-Me.prototype.solveComplexCaptcha = function (captcha) {
-  return parseStringIntoCaptcha(captcha)
-    .filter(matchingHalfwayRoundDigit)
-    .reduce((sum, current) => (sum + current), 0)
-}
+  // Day 1 stuff
+  solveSimpleCaptcha (captcha) {
+    return this.parseStringIntoCaptcha(captcha)
+      .filter(this.matchingNextArrayDigit)
+      .reduce((sum, current) => (sum + current), 0)
+  }
 
-function parseStringIntoCaptcha(captcha) {
-  return captcha
-    .replace(/\n|\r/g, '')
-    .split('')
-    .map(e => parseInt(e))
-}
+  solveComplexCaptcha (captcha) {
+    return this.parseStringIntoCaptcha(captcha)
+      .filter(this.matchingHalfwayRoundDigit)
+      .reduce((sum, current) => (sum + current), 0)
+  }
 
-function matchingNextArrayDigit(element, index, array) {
-  return element == array[(index+1) % array.length]
-}
+  parseStringIntoCaptcha(captcha) {
+    return captcha
+      .replace(/\n|\r/g, '')
+      .split('')
+      .map(e => parseInt(e))
+  }
 
-function matchingHalfwayRoundDigit(element, index, array) {
-  return element == array[(index+ array.length / 2) % array.length]
+  matchingNextArrayDigit(element, index, array) {
+    return element == array[(index+1) % array.length]
+  }
+
+  matchingHalfwayRoundDigit(element, index, array) {
+    return element == array[(index+ array.length / 2) % array.length]
+  }
 }
 
 module.exports = Me
